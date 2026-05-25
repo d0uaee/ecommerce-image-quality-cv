ba2773de82a1d92d8126fa9941dfc08ed22cd17e
d0ouaee <ahadjidouae@gmail.com>
2026-05-26 00:54:41 +0100

    Simplify src package init for analyzer imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,2 @@
-from src.detector import detect_product
-from src.analyzer import analyze_image, global_score
-from src.ocr import extract_text
-from src.nlp import extract_entities
-from src.caption import generate_listing
-from src.n8n_search import search_competitors
+"""Core package for the zero-shot e-commerce image quality project."""
 
-__all__ = [
-    "detect_product",
-    "analyze_image",
-    "global_score",
-    "extract_text",
-    "extract_entities",
-    "generate_listing",
-    "search_competitors",
-]
